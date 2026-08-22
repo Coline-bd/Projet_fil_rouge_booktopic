4e69a7b11463182f6431e1102105883c6d252ff4
coline-bd <coline.bertrand2@gmail.com>
2026-08-22 20:35:54 +0200

    feat: creation des class User et UserRepository

---
+++ app/models/user.php
@@ -0,0 +1,100 @@
+<?php
+
+class User
+{
+    //attributs
+    private int $id;
+    private string $firstname;
+    private string $lastname;
+    private string $login;
+    private string $email;
+    private string $birthdate;
+
+    private ?string $picture;
+    private ?string $presentation;
+
+    private int $roleId;
+
+    private ?array $categories;
+
+    //construct
+    public function __construct(
+        int $id,
+        string $firstname,
+        string $lastname,
+        string $login,
+        string $email,
+        string $birthdate,
+        ?string $picture,
+        ?string $presentation,
+        ?array $categories,
+        int $roleId
+    ) {
+        $this->id = $id;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->login = $login;
+        $this->email = $email;
+        $this->birthdate = $birthdate;
+        $this->picture = $picture;
+        $this->presentation = $presentation;
+        $this->categories = $categories;
+        $this->roleId = $roleId;
+    }
+
+    //getter and setter
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getBirthdate(): string
+    {
+        return $this->birthdate;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function getPresentation(): ?string
+    {
+        return $this->presentation;
+    }
+
+    public function getRoleId(): int
+    {
+        return $this->roleId;
+    }
+
+    public function getCategories(): array
+    {
+        return $this->categories;
+    }
+
+    public function setCategory(string $category): void
+    {
+        $this->categories[] = $category;
+    }
+}
---
+++ app/models/userRepository.php
@@ -0,0 +1,59 @@
+<?php
+
+class UserRepository{
+    //atributes
+    private PDO $pdo;
+
+    //construct
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    //methods
+    public function findById(int $id): ?User{
+        try{
+            $req=$this->pdo->prepare("SELECT u.id_user,firstname_user,login_user,lastname_user,picture_user,presentation_user,GROUP_CONCAT(name_category) as categories FROM `user` as u 
+            JOIN user_category as uc ON u.id_user=uc.id_user 
+            JOIN category as c ON uc.id_category=c.id_category WHERE u.id_user='?' 
+            GROUP BY u.id_user;");
+            $req->bindValue(1,$id,pdo::PARAM_INT);
+            $req->execute();
+            $data=$req->fetchAll(pdo::FETCH_ASSOC);
+            
+            if (empty($data)){
+                return null;
+            }
+            $data["categories"]=$data["categories"]? explode(",", $data["categories"]): []; //if several categories
+            $user= new User($data["id_user"],$data["firstname_user"],$data["lastname_user"],$data["login_user"],$data["mail_user"],$data["birthdate_user"],$data["picture_user"],$data["presentation_user"],$data["categories"],$data["id_role"]);
+            return $user;
+        }
+        catch(Exception $e){
+            die($e->getMessage());
+        }
+    }
+
+    public function findAll():?array{
+        try{
+            $req=$this->pdo->prepare("SELECT u.id_user,firstname_user,login_user,lastname_user,picture_user,presentation_user,name_category FROM `user` as u 
+            JOIN user_category as uc ON u.id_user=uc.id_user 
+            JOIN category as c ON uc.id_category=c.id_category");
+            $req->execute();
+            $data=$req->fetchAll(pdo::FETCH_ASSOC);
+            
+            if (empty($data)){
+                return null;}
+
+            $users=[];
+            foreach($data as $value){
+                $value["categories"]=$value["categories"]? explode(",", $value["categories"]): []; //if several categories
+                $users[]=new User($value["id_user"],$value["firstname_user"],$value["lastname_user"],$value["login_user"],$value["mail_user"],$value["birthdate_user"],$value["picture_user"],$value["presentation_user"],$value["categories"],$value["id_role"]);
+            }
+            return $users;
+        }
+        catch(Exception $e){
+            die($e->getMessage());
+        }
+    }
+}
+
